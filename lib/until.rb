4c08e3b7374238627a59ba2f5bdd3d72b9c25d26
Oscar Munoz <omunozg@emory.edu>
2019-03-10 03:51:07 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,8 @@
 def using_until
         levitation_force = 6
-while levitation_force < 10
-puts ("Wingardium Leviosa")
-levitation_force += 1
+        until levitation_force = 10 
+puts("Wingardium Leviosa")
+levitation_force += 10
 end
-    
 end
 
